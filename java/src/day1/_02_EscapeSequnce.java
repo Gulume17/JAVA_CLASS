32e29578aec14785cbba790d903c23df081c6555
Kyeongyunhui <yunhui72659147@gmail.com>
2025-02-04 16:49:11 +0900

    day1 final

---
+++ java/src/day1/_02_EscapeSequnce.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class _02_EscapeSequnce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Write\n\nonce!"); // 즐 바꿈
+		System.out.println("사과\t배\t오렌지\t"); // 탭
+//		System.out.println("apple\tpear\torange"); // ctrl + / : 주석 처리 
+		System.out.println("우리집 강쥐는 \"멍멍\" 짖어요"); // 큰 따옴표 문자열 처리
+		
+		// 한줄 주석입니다.
+		
+		/*
+		 * 여러줄 주석입니다.
+		 * 
+		 */
+
+	}
+
+}
