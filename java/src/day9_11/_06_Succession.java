7f18fd3ec7e5a12a91e6d187c3c2b1350c02332d
Kyeongyunhui <yunhui72659147@gmail.com>
2025-02-19 16:52:26 +0900

    day12

--- java/src/day9_11/_06_Succession.java
+++ java/src/day9_11/_06_Succession.java
@@ -10,6 +10,21 @@ class Dog extends Animal{
 	void sound() {
 		System.out.println("멍멍멍!!!");
 	}
+	
+	@Override
+	void eat() {
+		System.out.println("밥을먹자 멍멍멍~~~");
+	}
+}
+
+class Cat extends Animal{
+	void sound() {
+		System.out.println("냐아아아옹");
+	}
+	@Override
+	void eat() {
+		System.out.println("밥을먹자 냥냥냥~~~");
+	}
 }
 
 public class _06_Succession {
@@ -18,6 +33,9 @@ public class _06_Succession {
 		
 		Dog myDog = new Dog();
 		myDog.eat();
+		
+		Cat myCat = new Cat();
+		myCat.eat();
 		
 	}
 
