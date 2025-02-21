feb13d596f136f121c18b9d0ee2b170a7d5b3727
Kyeongyunhui <yunhui72659147@gmail.com>
2025-02-21 16:57:00 +0900

    day14

--- java/src/day9_13/_04_AbstractClassFood.java
+++ java/src/day9_13/_04_AbstractClassFood.java
@@ -2,20 +2,20 @@ package day9_13;
 
 abstract class Food{
 	abstract void cook();
-	}
+}
 
 class Pizza extends Food{
 	@Override
 	void cook() {
-		System.out.println("오븐에서 피자 굽기");
-	}
-	
-class Ramen extends Food{
-	@Override
-	 void cook() {
-		System.out.println("보글보글 맛좋은 라면");
+		System.out.println("오븐에서 피자굽기");
 	}
 }
+
+class Ramen extends Food{
+	@Override
+	void cook() {
+		System.out.println("보글보글 맛좋은라면");
+	}
 }
 
 public class _04_AbstractClassFood {
@@ -24,9 +24,9 @@ public class _04_AbstractClassFood {
 		/*
 		 * 요리방법(추상메서드)이 들어있는 추상클래스 만들기
 		 * 피자클래스 - 추상클래스 상속받기
-		 * 라면 클래스 - 추상클래스 상속받기
+		 * 라면클래스 - 추상클래스 상속받기
 		 * 
-		 */
+		 * */
 		
 		Food myFood = new Pizza();
 		myFood.cook();
